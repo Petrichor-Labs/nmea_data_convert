48df819ad57a7390c53e3a19c6cc851fa58f289a
Emiel Demeestere <emiel.demeestere@ugent.be>
2023-04-20 20:31:14 +0200

    Add merged test data and add more table names

--- db_table_lists.py
+++ db_table_lists.py
@@ -1,4 +1,11 @@
-nmea_tables = [
+NMEA_TABLES = [
+    # GPS
+    'nmea_gp_gga',
+    'nmea_gp_gsa',
+    'nmea_gp_gsv',
+    'nmea_gp_rmc',
+    'nmea_gp_vtg',
+    # GLONAS
     'nmea_gl_gsv',
     'nmea_gn_gga',
     'nmea_gn_gll',
@@ -7,5 +14,10 @@ nmea_tables = [
     'nmea_gn_gst',
     'nmea_gn_rmc',
     'nmea_gn_vtg',
-    'nmea_gp_gsv',
+    # Galileo
+    'nmea_ga_gga',
+    'nmea_ga_gsa',
+    'nmea_ga_gsv',
+    'neam_ga_rmc',
+    'nmea_ga_vtg',
 ]
